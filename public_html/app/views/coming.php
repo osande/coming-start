4291878cae3c6759fd09104d9417fdf0ca92a575
José Proença <jose.proenca@owesome.pt>
2020-03-11 00:57:21 +0000

    Add coming sample

--- public_html/app/views/coming.php
+++ public_html/app/views/coming.php
@@ -1,8 +1,76 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <title>Plus Coming Soon Template </title>
+        <link rel="shortcut icon" href="assets/img/favicon.ico" /> <!--PUT YOUR FAVICON HERE-->
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <style>
+            .carousel-item {
+                height: 100vh;
+                min-height: 350px;
+                background: no-repeat center center scroll;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <!-- Slide One - Set the background image for this slide in the line below -->
+                    <div class="carousel-item active" style="background-image: url('/assets/img/portfolio/fullsize/1.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">First Slide</h2>
+                            <p class="lead">This is a description for the first slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Two - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('/assets/img/portfolio/fullsize/2.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">Second Slide</h2>
+                            <p class="lead">This is a description for the second slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Three - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('/assets/img/portfolio/fullsize/3.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-4">Third Slide</h2>
+                            <p class="lead">This is a description for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </header>
 
+        <!-- END MAIN CONTAINER -->
+        <!--REQUIRED SCRIPTS FILES-->  <!--SCRIPTS AT END SO PAGE WILL TAKE LESS TIME TO LOAD-->
+        <!--REQUIRED CORE JQUERY SCRIPTS FILE--> 
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
