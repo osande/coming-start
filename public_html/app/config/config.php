5d7cd37f02b76aaa9b3f69736863805ca4609023
José Proença <jose.proenca@owesome.pt>
2020-03-28 02:57:57 +0000

    update

--- public_html/app/config/config.php
+++ public_html/app/config/config.php
@@ -391,12 +391,13 @@ $config['encryption_key'] = '1wRAikoc7tYlFrkS96c3zMtKGseBD51f';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver']      = 'database';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_expiration']  = 7200;
+$config['sess_save_path']   = 'ci_sessions';
+$config['sess_match_ip']    = FALSE;
+
+$config['sess_time_to_update']     = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
